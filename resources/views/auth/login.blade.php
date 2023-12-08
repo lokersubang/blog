ae42f0781e7c5ae744d49f554728995e9eb1ffa6
Dadan Nurmaulana <dadannurmaulana@gmail.com>
2023-12-08 22:28:02 +0700

    Membuat login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,35 @@
-<x-guest-layout>
+<x-auth>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
     <form method="POST" action="{{ route('login') }}">
         @csrf
+        <a href="{{ route('home') }}"> <img class="mb-4" src="/img/login.png" alt="Login" title="Home" width="72"
+                height="72"></a>
+        <h1 class="h3 mb-3 fw-normal">Login <strong>Akun</strong></h1>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+        <div class="form-floating">
+            <input type="email" class="form-control" id="email" type="email" name="email"
+                :value="old('email')" required autofocus autocomplete="username">
+            <label for="email">Email address</label>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
+        <div class="form-floating">
+            <input type="password" class="form-control" id="password" placeholder="Password" type="password"
+                name="password" required autocomplete="current-password">
+            <label for="password">Password</label>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+        <div class="form-check text-start my-3">
+            <input class="form-check-input" id="remember_me" type="checkbox" value="remember-me" name="remember"
+                id="flexCheckDefault">
+            <label class="form-check-label" for="remember_me">
+                Remember me
             </label>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
+        <button class="btn btn-primary w-100 py-2 fw-semibold" type="submit">Login</button>
+        <p class="mt-5 mb-3 text-body-secondary">&copy; {{ date('Y') }} Blog</p>
     </form>
-</x-guest-layout>
+
+</x-auth>
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', HomeController::class)->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/test','layouts.auth');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
